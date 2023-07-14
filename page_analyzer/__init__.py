87481784c6dfc51d725b174d47615042ef5fb21c
Anisimoff <happyprooo@gmail.com>
2023-07-14 20:15:19 +0300

    step 3 fix 2

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -1,6 +1,9 @@
 from page_analyzer.app import app # noqa F401
+from page_analyzer.validator import url_validator, url_normalize
 
 
 __all__ = (
-    'app'
+    'app',
+    'url_validator',
+    'url_normalize'
 )
