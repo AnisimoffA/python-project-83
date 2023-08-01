6ef47279d84eab3f644636eb49772be91e54f68d
Anisimoff <happyprooo@gmail.com>
2023-08-01 19:29:35 +0300

    last fix

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,21 +1,18 @@
 from dotenv import load_dotenv
 from psycopg2.extras import NamedTupleCursor
 import psycopg2
+from psycopg2 import pool # NOQA F401
 import os
 
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
-
-
-def connect_db():
-    conn = psycopg2.connect(DATABASE_URL)
-    return conn
+connect_db = psycopg2.pool.SimpleConnectionPool(1, 20, DATABASE_URL)
 
 
 def insert_into_db(requirement, values):
     try:
-        connection = connect_db()
+        connection = connect_db.getconn()
         connection.autocommit = True
 
         with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
@@ -25,13 +22,13 @@ def insert_into_db(requirement, values):
         print('[INFO]Ошибка: ', Ex)
     finally:
         if connection:
-            connection.close()
+            connect_db.putconn(connection)
             print('[INFO]Соединение закрыто')
 
 
 def select_one_from_db(requirement, values):
     try:
-        connection = connect_db()
+        connection = connect_db.getconn()
         connection.autocommit = True
 
         with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
@@ -43,14 +40,14 @@ def select_one_from_db(requirement, values):
         print('[INFO]Ошибка: ', Ex)
     finally:
         if connection:
-            connection.close()
+            connect_db.putconn(connection)
             print('[INFO]Соединение закрыто')
             return data
 
 
 def select_many_from_db(requirement, values):
     try:
-        connection = connect_db()
+        connection = connect_db.getconn()
         connection.autocommit = True
 
         with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
@@ -62,6 +59,6 @@ def select_many_from_db(requirement, values):
         print('[INFO]Ошибка: ', Ex)
     finally:
         if connection:
-            connection.close()
+            connect_db.putconn(connection)
             print('[INFO]Соединение закрыто')
             return data
